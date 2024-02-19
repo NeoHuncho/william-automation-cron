0d96897042d704abae9cad8bfc41306b2309579b
William Guinaudie <william.guinaudie@gmail.com>
2024-02-19 22:35:05 +0100

    improved diary entry prompt

--- src/voiceMemosAutomation/constants/constants.ts
+++ src/voiceMemosAutomation/constants/constants.ts
@@ -1,7 +1,6 @@
-export const voiceRecordingDirectory = '/voice recording';
+export const voiceRecordingDirectory = '/delete';
 export const diaryEntryOrDreamPrompt = (type: string) =>
-  `In markdown, first provide a detailed title summarizing the entry in h1. then provide 2-3 bullet points summarizing my ${type} and then a revised version of the text. Keep the same tone and vocabulary. KEEP Do not add unnecessary information or titles.`;
+  `In markdown, first provide a detailed title summarizing the entry in h1. then provide 2-3 bullet points summarizing my ${type}. then provide a  revised lengthy version of the text, that does not omit any details. Keep the same tone and vocabulary. KEEP Do not add unnecessary information or titles.`;
 export const documentationPrompt = `from the text below, generate a title. the title should be prefixed and suffixed by a !. Then, in markdown, make a bulleted list of the main points of the text. Do not miss out anything mentioned in the text.`;
 export const textCleaned =
   'from the text below, add punctuation. do not add any word that is not present. keep all the detail shared in the entry.';
-

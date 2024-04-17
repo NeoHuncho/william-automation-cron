3b0224dc92f3bcd584feb93133fdf1a0f2f2641a
William Guinaudie <william.guinaudie@gmail.com>
2024-04-17 19:42:20 +0200

    v1.0.4 (optimized LLM prompts)

--- src/voiceMemosAutomation/constants/constants.ts
+++ src/voiceMemosAutomation/constants/constants.ts
@@ -1,6 +1,6 @@
 export const voiceRecordingDirectory = () =>
   process.env.NODE_ENV !== 'production' ? '/devVoiceMemos' : '/voice recording';
 export const diaryEntryOrDreamPrompt = (type: string) =>
-  `In markdown, first provide a detailed title summarizing the entry in h1. then provide 2-3 bullet points summarizing my ${type}. then provide a  revised lengthy version of the text, that does not omit any details. Keep the same tone and vocabulary. Do not add unnecessary information or titles. Respond in the language of the following text.`;
+  `In markdown, first provide a detailed title in H1, summarizing the different topics shared in the text. then provide 2-3 bullet points summarizing my ${type}. then provide a  revised lengthy version of the text, that does not omit any details. Keep the same tone and vocabulary. Do not add unnecessary information or titles. Respond in the language of the following text.`;
 export const textCleaned =
   'from the text below, add punctuation. do not add any word that is not present. keep all the detail shared in the entry.';
